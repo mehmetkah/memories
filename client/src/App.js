826218dff6e16b561a005435e93d17b2d1ecd22a
“mehmetkah” <mehmetkahca@gmail.com>
2023-04-01 03:28:21 -0600

    client side folder setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import ReactDom from "react-dom";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 const App = () => {
   return (
     <div>
